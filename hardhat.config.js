01de5c4848b157032f2d5cf5e9e2cd98d4657dc0
nayanchoudhary31 <nayanscsit09@gmail.com>
2022-08-08 22:52:15 +0530

    Added Rinkeby Network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,20 +1,26 @@
 require("@nomicfoundation/hardhat-toolbox");
-require('dotenv').config()
+require("dotenv").config();
 require("@nomiclabs/hardhat-etherscan");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.4",
-  networks:{
-    goerli:{
-      url:`
+  networks: {
+    rinkeby: {
+      url: `
+      https://rinkeby.infura.io/v3/${process.env.INFURA_KEY}`,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    goerli: {
+      url: `
       https://goerli.infura.io/v3/${process.env.INFURA_KEY}`,
-      accounts:[process.env.PRIVATE_KEY]
-    }
+      accounts: [process.env.PRIVATE_KEY],
+    },
   },
-  etherscan:{
-    apiKey:{
-      goerli:`${process.env.API_KEY}`
-    }
-  }
+  etherscan: {
+    apiKey: {
+      goerli: `${process.env.API_KEY}`,
+      rinkeby: `${process.env.API_KEY}`,
+    },
+  },
 };
